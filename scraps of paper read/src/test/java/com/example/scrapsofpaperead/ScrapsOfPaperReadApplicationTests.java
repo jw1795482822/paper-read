df12bd921a2fc804a7b66cb30e9a8068b9a6d616
jw1795482822 <133122677+jw1795482822@users.noreply.github.com>
2023-05-20 11:48:11 +0800

    Add files via upload

---
+++ scraps of paper read/src/test/java/com/example/scrapsofpaperead/ScrapsOfPaperReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scrapsofpaperead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapsOfPaperReadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
