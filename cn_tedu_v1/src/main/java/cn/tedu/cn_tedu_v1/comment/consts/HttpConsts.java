4038d80a2b76c76ec3c33e4090aa61eb960e875d
bmy <1129729148@qq.com>
2023-06-30 14:46:31 +0800

    登陆注册功能增加公共配置,增加拦截器

---
+++ cn_tedu_v1/src/main/java/cn/tedu/cn_tedu_v1/comment/consts/HttpConsts.java
@@ -0,0 +1,21 @@
+package cn.tedu.cn_tedu_v1.comment.consts;
+
+/**
+ * HTTP相关常量
+ *
+ * @author java@tedu.cn
+ * @version 1.0
+ */
+public interface HttpConsts {
+
+    /**
+     * 请求头：授权信息
+     */
+    String HEADER_AUTHORIZATION = "Authorization";
+
+    /**
+     * 请求头：客户端浏览器
+     */
+    String HEADER_USER_AGENT = "User-Agent";
+
+}
---
+++ cn_tedu_v1/src/main/java/cn/tedu/cn_tedu_v1/core/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,125 @@
+package cn.tedu.cn_tedu_v1.core.filter;
+
+
+
+import cn.tedu.cn_tedu_v1.comment.consts.HttpConsts;
+import com.alibaba.fastjson.JSON;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+/**
+ * <p>处理JWT的过滤器</p>
+ *
+ * <p>此过滤器的主要作用：</p>
+ * <ul>
+ *     <li>尝试接收客户端的请求中携带的JWT数据</li>
+ *     <li>尝试解析JWT数据</li>
+ *     <li>将解析得到的用户数据创建为Authentication对象，并存入到SecurityContext中</li>
+ * </ul>
+ *
+ * @author java@tedu.cn
+ * @version 1.0
+ */
+@Slf4j
+@Component
+public class JwtAuthorizationFilter extends OncePerRequestFilter implements HttpConsts {
+
+    @Value("${tea-store.jwt.secret-key}")
+    private String secretKey;
+
+    /**
+     * JWT最小长度值
+     */
+    public static final int JWT_MIN_LENGTH = 113;
+
+    public JwtAuthorizationFilter() {
+        log.info("创建过滤器对象：JwtAuthorizationFilter");
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.debug("处理JWT的过滤器开始处理当前请求……");
+        // 尝试接收客户端的请求中携带的JWT数据
+        // 业内惯用的做法是：客户端会将JWT放在请求头中名为Authorization的属性中
+        String jwt = request.getHeader(HEADER_AUTHORIZATION);
+        log.debug("客户端携带的JWT：{}", jwt);
+
+        // 判断JWT的基本有效性（没有必要尝试解析格式明显错误的JWT数据）
+        if (!StringUtils.hasText(jwt) || jwt.length() < JWT_MIN_LENGTH) {
+            // 对于无效的JWT，应该直接放行
+            log.warn("当前请求中，客户端没有携带有效的JWT，将放行");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 尝试解析JWT数据
+        log.debug("尝试解析JWT数据……");
+        response.setContentType("application/json; charset=utf-8");
+        Claims claims = null;
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(jwt)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            log.warn("解析JWT时出现异常：ExpiredJwtException");
+            String message = "操作失败，您的登录信息已经过期，请重新登录！";
+
+        } catch (SignatureException e) {
+            log.warn("解析JWT时出现异常：SignatureException");
+            String message = "非法访问，你的本次操作已经被记录！";
+
+        } catch (MalformedJwtException e) {
+            log.warn("解析JWT时出现异常：MalformedJwtException");
+            String message = "非法访问，你的本次操作已经被记录！";
+
+        } catch (Throwable e) {
+            log.warn("解析JWT时出现异常：{}", e);
+            String message = "服务器忙，请稍后再试！【同学们，看到这句时，你应该检查服务器端的控制台，并在JwtAuthorizationFilter中解析JWT时添加处理异常的catch代码块】";
+
+        }
+
+        // 从解析结果中获取用户的信息
+        Long id = claims.get("id", Long.class);
+        String username = claims.get("username", String.class);
+        String authoritiesJsonString = claims.get("authoritiesJsonString", String.class);
+        log.debug("JWT中的用户id = {}", id);
+        log.debug("JWT中的用户名 = {}", username);
+        log.debug("JWT中的用户权限列表 = {}", authoritiesJsonString);
+
+        // 将解析得到的用户数据创建为Authentication对象
+        CurrentPrincipal principal = new CurrentPrincipal(); // 当事人
+        principal.setId(id);
+        principal.setUsername(username);
+        Object credentials = null; // 凭证：无需凭证
+        List<SimpleGrantedAuthority> authorities
+                = JSON.parseArray(authoritiesJsonString, SimpleGrantedAuthority.class);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                principal, credentials, authorities);
+
+        // 将Authentication对象存入到SecurityContext中
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+
+        // 过滤器链继续执行，即：放行
+        filterChain.doFilter(request, response);
+    }
+
+}
